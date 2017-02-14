4853ec5f0f0db3b58bc6562ccef09798bb293e13
Julien Aimonier-Davat <juju7397@gmail.com>
2017-02-14 14:03:10 +0100

    ajout src

--- src/Puiss4Frame.java
+++ src/Puiss4Frame.java
@@ -1,4 +1,18 @@
+import java.awt.Frame;
 
-public class Puiss4Frame {
-
+public class Puiss4Frame extends Frame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	// Attributes
+	private static final int LARGEUR = 800;
+	private static final int HAUTEUR = 500;
+	
+	// Method
+	public Puiss4Frame() {
+		this.setTitle("Puissance 4");
+		this.setSize(LARGEUR,HAUTEUR);
+		
+	}
 }
