78f271e1ac67481e8320ea333dfdc3553a168bb9
Julien Aimonier-Davat <juju7397@gmail.com>
2017-02-14 14:40:14 +0100

    modif frame

--- src/Puiss4Frame.java
+++ src/Puiss4Frame.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 
+import puisQuatre.Puis4;
+
 public class Puiss4Frame extends Frame {
 	/**
 	 * 
@@ -8,13 +10,16 @@ public class Puiss4Frame extends Frame {
 	// Attributes
 	private static final int LARGEUR = 800;
 	private static final int HAUTEUR = 500;
-	
+	private Puis4 modele;
 	// Method
-	public Puiss4Frame() {
+	public Puiss4Frame(Puis4 modele) {
+		this.modele = modele;
 		this.setTitle("Puissance 4");
 		this.setSize(LARGEUR,HAUTEUR);
 		this.setLayout(new BorderLayout());
 		this.setBackground(Color.GRAY);
+		
+		Canvas grillePuiss4Canvas = new grillePuiss4Canvas(modele);
 		
 		this.setVisible(true);
 	}
