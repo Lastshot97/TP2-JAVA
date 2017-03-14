04aa7ccd963da8be66b1a437268ae7374a46fcbb
Julien Aimonier-Davat <juju7397@gmail.com>
2017-03-14 15:25:33 +0100

    Amélioration du rendu graphique
    Ajout d'images aux fenêtres de dialogue
    Modification de l'aspets des boutons de la fenêtre principale

--- src/Puis4Dialog.java
+++ src/Puis4Dialog.java
@@ -1,10 +1,13 @@
 import java.awt.BorderLayout;
 import java.awt.Button;
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import puisQuatre.Puis4;
 import java.awt.Dialog;
+import java.awt.Image;
 import java.awt.Panel;
+import java.io.IOException;
 
 public class Puis4Dialog extends Dialog{
 	
@@ -14,14 +17,22 @@ public class Puis4Dialog extends Dialog{
 		super(frame, "PARTIE TERMINEE", false);
 
 		JLabel endGame;  // JLABEL ?
-		
+		Image tryAgain = null;
+		Image winner = null;
+		try{
+			tryAgain = ImageIO.read(this.getClass().getResourceAsStream("TryAgain.jpg"));
+			winner = ImageIO.read(this.getClass().getResourceAsStream("YouWin.jpg"));
+		} catch (IOException e){
+			System.err.println("Erreur lors de la lecture des images de la classe PuisDialog");
+			e.printStackTrace();
+		}
 		if (modele.estJoueurGagne()){
 			// PlayMusic.play("win.wav");  // TODO implementer
-			endGame = new JLabel(new ImageIcon("D:/Pictures/Projets/YouWin.jpg"));	// FIXME mettre une valeur local	
+			endGame = new JLabel(new ImageIcon(winner));	// FIXME mettre une valeur local	
 			this.setSize(280,270);
 		} else{
 			// PlayMusic.play("lose.wav");  //TODO implementer
-			endGame = new JLabel(new ImageIcon("D:/Pictures/Projets/TryAgain.jpg"));
+			endGame = new JLabel(new ImageIcon(tryAgain));
 			this.setSize(420,270);
 		}
 		
--- src/Puiss4Frame.java
+++ src/Puiss4Frame.java
@@ -4,13 +4,14 @@ import puisQuatre.Puis4;
 
 
 public class Puiss4Frame extends Frame  {
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
+	
 	// Attributes
 	private static final int LARGEUR = 900;
 	private static final int HAUTEUR = 600;
+	private Button abandonner;
+	private Button goMenu;
 	private GrillePuiss4Canvas grille;
 	
 	// Method
@@ -22,19 +23,26 @@ public class Puiss4Frame extends Frame  {
 		PlayMusic.play("jeu.wav");
 
 		grille = new GrillePuiss4Canvas(modele,this);	
-	    Button abandonner = new Button ("Abandonner");
-	    abandonner.setBackground(Color.GRAY);
-	    Label niveau = new Label("niveau : " + modele.getNiveauJeu());
-	    Panel bottom = new Panel(new FlowLayout(FlowLayout.CENTER));
-	    bottom.add(abandonner);
-	    bottom.add(niveau);
+		
+	    abandonner = new Button ("Abandonner");
+	    abandonner.setBackground(new Color(133,133,133));
+	    
+	    goMenu = new Button ("Retour au Menu");	
+	    goMenu.setBackground(new Color(133,133,133));
+	    
+	    Panel bottom = new Panel();
+	    bottom.setLayout(new GridLayout(1,5));
+	    bottom.setPreferredSize(new Dimension(LARGEUR,30));
+	    bottom.add(new Panel()); bottom.add(abandonner); bottom.add(new Panel()); bottom.add(goMenu); bottom.add(new Panel());
+
 	    bottom.setBackground(Color.DARK_GRAY);
 	      
 	    this.add(grille, BorderLayout.CENTER);
 	    this.add(bottom, BorderLayout.SOUTH);
 	    
-	    abandonner.addActionListener(new ButtonAbandonListener(modele,grille));
-	    grille.addMouseListener(new grillePuiss4Listener(modele,grille)); 
+	    abandonner.addActionListener(new ButtonAbandonListener(modele, grille));
+	    goMenu.addActionListener(new goMenuListener(this, modele));
+	    grille.addMouseListener(new grillePuiss4Listener(modele, grille)); 
 	    this.addWindowListener(new CloseWindowListener(this));
 	    
 		this.setVisible(true);
